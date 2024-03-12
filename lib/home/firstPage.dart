90117d9fdd8c2a90709c5b82b8ecfc1007ef320c
Atom2544 <zylop321@gmail.com>
2024-03-12 10:39:35 +0700

    fix

--- lib/home/firstPage.dart
+++ lib/home/firstPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'package:possodexo/home/homePage.dart';
+import 'package:possodexo/seconDisplay/seconmain.dart';
 import 'package:possodexo/home/widgets/ItemMenuWidget.dart';
 
 class FirstPage extends StatefulWidget {
@@ -155,9 +156,7 @@ class _FirstPageState extends State<FirstPage> {
                 margin: EdgeInsets.only(top: 2, right: 1),
                 padding: EdgeInsets.only(top: 0, right: 0, left: 0),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(1),
-                      topRight: Radius.circular(1)),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(1), topRight: Radius.circular(1)),
                   color: Color.fromARGB(255, 229, 230, 240),
                 ),
                 child: _pageView(),
